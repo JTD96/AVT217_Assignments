c138ee5b372024d9607ee788a2cd75b0c7505e67
Jake Durborow <jtdurb96@gmail.com>
2018-11-06 10:50:56 -0500

    notes

--- P3/js/topbutton.js
+++ P3/js/topbutton.js
@@ -1,4 +1,16 @@
+// $(document).ready(function() {
+	// 	run function on initial page load
+// });
+// $(window).resize(function(){
+	// 	run function on resize of the view window
+// });
+// $(window).scroll(function(){
+	// 	run when you scroll
+// });
+
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+//look up jQuery API for more function types
