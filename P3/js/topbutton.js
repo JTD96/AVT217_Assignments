10823e7f0abbbfba87486c8949da0410c7b40349
Jake Durborow <jtdurb96@gmail.com>
2018-11-06 10:52:25 -0500

    Update P3/js/topbutton.js

--- P3/js/topbutton.js
+++ P3/js/topbutton.js
@@ -1,12 +1,12 @@
 // $(document).ready(function() {
 	// 	run function on initial page load
-// });
 // $(window).resize(function(){
 	// 	run function on resize of the view window
 // });
 // $(window).scroll(function(){
 	// 	run when you scroll
 // });
+//});
 
 function topFunction() {
     document.body.scrollTop = 0;
